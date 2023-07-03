d4e8007654cd613336131ed6e92051aa2249ac0f
Mohamed alshami <mo.mohamed45987@gmail.com>
2023-07-03 08:52:55 +0300

    Updating hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,14 @@
 function Hero() {
   return (
-    <section className="text-white flex flex-col justify-center container mx-auto p-6 min-h-screen pt-20 xl:pt-0">
+    <section className="text-white flex flex-col justify-center container mx-auto p-6 min-h-screen pt-16 xl:pt-0">
       <h1 className="text-orange mb-7 ">Hi my name is</h1>
-      <div className="text-clamp font-semibold min-w-max">Mohamed Elshamy.</div>
-      <div className="text-clamp font-semibold mt-1.5 max-w-[1200px]">
+      <div className="text-clamp font-semibold min-w-max text-headerColor">
+        Mohamed Elshamy.
+      </div>
+      <div className="text-clamp font-semibold mt-1.5 max-w-[1200px] text-textColor">
         I build things for the web.
       </div>
-      <div className="max-w-[650px] mt-5 text-[18px]">
+      <div className="max-w-[650px] mt-5 text-[18px] text-textColor">
         I'm a software engineer specializing in building exceptional digital
         experiences for the web. I write both front-end and back-end code.
       </div>
